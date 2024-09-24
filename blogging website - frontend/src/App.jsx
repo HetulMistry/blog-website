23fd0644c7a72aaae0cf1057a27d63b9ecdf838a
Hetul Mistry <Manishcmdgm812@gmail.com>
2024-09-24 18:44:05 +0530

    Initial Commit

---
+++ blogging website - frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar.component";
+import UserAuthForm from "./pages/userAuthForm.page";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route path="sign-in" element={<UserAuthForm type={"sign-in"} />} />
+        <Route path="sign-up" element={<UserAuthForm type={"sign-up"} />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
---
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -0,0 +1,142 @@
+import { Link } from "react-router-dom";
+import { useRef } from "react";
+import { Toaster, toast } from "react-hot-toast";
+import axios from "axios";
+import InputBox from "../components/input.component";
+import googleIcon from "../imgs/google.png";
+import AnimationWrapper from "../common/page-animation";
+
+const UserAuthForm = ({ type }) => {
+  const authForm = useRef();
+
+  const userAuthThroughServer = (serverRoute, formData) => {
+    console.log(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, formData);
+    axios
+      .post("http://localhost:5173/sign-up")
+      .then(({ data }) => {
+        console.log(data);
+      })
+      .catch(({ response }) => {
+        toast.error(response.data.error);
+      });
+  };
+
+  const handleSubmit = (e) => {
+    let serverRoute = type == "sign-in" ? "/sign-in" : "/sign-up";
+
+    e.preventDefault();
+
+    let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+
+    let form = new FormData(authForm.current);
+    let formData = {};
+
+    for (let [key, value] of form.entries()) {
+      formData[key] = value;
+    }
+
+    let { fullname, email, password } = formData;
+
+    if (fullname) {
+      if (fullname.length < 3) {
+        return toast.error("Fullname must be at least 3 letters long.");
+      }
+    }
+    if (!email.length) {
+      return toast.error("Enter email.");
+    }
+
+    if (!emailRegex.test(email)) {
+      return toast.error("Email is invalid.");
+    }
+
+    if (!passwordRegex.test(password)) {
+      return toast.error(
+        "Password should be 6 to 20 characters long with a numeric, 1 lowercase and 1 uppercase letters."
+      );
+    }
+
+    userAuthThroughServer(serverRoute, formData);
+  };
+
+  return (
+    <AnimationWrapper keyValue={type}>
+      <section className="h-cover flex items-center justify-center">
+        <Toaster />
+        <form ref={authForm} className="w-[80%] max-w-[400px]">
+          <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
+            {type == "sign-in" ? "Welcome Back" : "Join Us Today"}
+          </h1>
+          {type != "sign-in" ? (
+            <InputBox
+              name={"fullname"}
+              type={"text"}
+              placeholder={"Full Name"}
+              icon={"fi-rr-user"}
+            />
+          ) : (
+            ""
+          )}
+
+          <InputBox
+            name={"email"}
+            type={"email"}
+            placeholder={"Email"}
+            icon={"fi-rr-envelope"}
+          />
+
+          <InputBox
+            name={"password"}
+            type={"password"}
+            placeholder={"Password"}
+            icon={"fi-rr-key"}
+          />
+
+          <button
+            className="btn-dark center mt-14"
+            type="submit"
+            onClick={handleSubmit}
+          >
+            {type.replace("-", " ")}
+          </button>
+
+          <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
+            <hr className="w-1/2 border-black" />
+            <p>OR</p>
+            <hr className="w-1/2 border-black" />
+          </div>
+
+          <button className="btn-dark capitalize flex items-center justify-center gap-4 w-[90%] center">
+            <img src={googleIcon} className="w-5" />
+            continue with google
+          </button>
+
+          {type == "sign-in" ? (
+            <p className="mt-6 text-dark-grey text-xl text-center">
+              Don't have an account?
+              <Link
+                to={"/sign-up"}
+                className="underline text-black text-xl ml-1"
+              >
+                Join Us Today
+              </Link>
+            </p>
+          ) : (
+            <p className="mt-6 text-dark-grey text-xl text-center">
+              Already have a member?
+              <Link
+                to={"/sign-in"}
+                className="underline text-black text-xl ml-1"
+              >
+                Sign In Here
+              </Link>
+            </p>
+          )}
+        </form>
+      </section>
+    </AnimationWrapper>
+  );
+};
+
+export default UserAuthForm;
